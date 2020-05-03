f7707ca3ff4bf9109d3ab82320c573a12d678871
Daniel Eades <danieleades@hotmail.com>
2020-05-03 21:59:37 +0100

    add clean-up to test

--- tests/container-create.rs
+++ tests/container-create.rs
@@ -2,12 +2,17 @@ use longshoreman::{Docker, Result};
 
 #[tokio::test]
 async fn test() -> Result<()> {
-    let containers = Docker::new().containers();
+    let docker = Docker::new();
+    let images = docker.images();
+    let containers = docker.containers();
+
+    let image = "alpine";
+
+    // Pull image
+    images.pull(image).tag("latest").send().await?;
 
     // Create a simple container
-    let id = containers.create("alpine").send().await?.id().clone();
-
-    println!("got this far");
+    let id = containers.create(image).send().await?.id().clone();
 
     containers.remove(&id).send().await?;
 
